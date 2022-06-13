a74b2e506f25c926d74bbb9d990ab656789d3909
alexis39tt <alessandro.fonzi@gmail.com>
2022-06-13 15:54:15 +0200

    changed RouteServiceProvider, HomeController and welcome.blade view to handle logged in users and display different page intead of home after login

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin', 'HomeController@index')->name('admin.home');
